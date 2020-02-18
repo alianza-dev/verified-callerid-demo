ceabfe20ded1a449cba677833b1342c383d991e5
M. Ryan Werner <mwerner@alianza.com>
2020-02-18 15:35:13 -0700

    moved sip credentials to ignored file

--- configuration.sh
+++ configuration.sh
@@ -3,6 +3,5 @@ export to_number=18018026412
 export cid_number=18017689999
 export key_file_name=captioncall_verified_callerid_prod.key
 export key_alias=captioncall_verified_callerid_prod
-export sip_username=mrw_softphone_3
-export sip_password=alianza
 export sip_proxy_server=sbcrtp.p2.sac.alianza.com:5065
+source ./sip_credentials.sh
